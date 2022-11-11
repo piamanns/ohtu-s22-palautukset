fd597df9daa8affe469ce7218ae6d8494f9ebd93
Pia Manns <pia.manns@gmail.com>
2022-11-11 14:03:30 +0200

    Add library for parsing TOML files

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,3 +1,4 @@
+import tomli
 from urllib import request
 from project import Project
 
@@ -9,7 +10,17 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+
+        try:
+            toml_dict = tomli.loads(content)
+        except tomli.TOMLDecodeError:
+            print("Invalid TOML file")
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+    
+        return Project(
+            toml_dict["tool"]["poetry"]["name"],
+            toml_dict["tool"]["poetry"]["description"],
+            toml_dict["tool"]["poetry"]["dependencies"],
+            toml_dict["tool"]["poetry"]["dev-dependencies"]
+        )
