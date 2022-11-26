f81b2e7cb9627503ca85e1cb40d7ecf1b661208a
Pia Manns <pia.manns@gmail.com>
2022-11-27 00:34:35 +0200

    Add test for buying two items of same product only resulting in one purchase object in cart
    At the same time change the purchases data type from list to dict, and fix the fact that the previous implementation actually did not increase the item count in an existing purchase object, when several units of the the same product was added (still passed all the tests, though.)

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,27 +4,30 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self._ostokset = []
+        self._ostokset = {}
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
         lkm = 0
-        for ostos in self._ostokset:
+        for ostos in self._ostokset.values():
             lkm += ostos.lukumaara()
         return lkm
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
         hinta = 0
-        for ostos in self._ostokset:
+        for ostos in self._ostokset.values():
             hinta += ostos.hinta()
         return hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-          self._ostokset.append(Ostos(lisattava))
+          if lisattava.nimi() in self._ostokset:
+              self._ostokset[lisattava.nimi()].muuta_lukumaaraa(1)
+          else:
+              self._ostokset[lisattava.nimi()] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -37,4 +40,4 @@ class Ostoskori:
     def ostokset(self):
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
-        return self._ostokset
+        return list(self._ostokset.values())
