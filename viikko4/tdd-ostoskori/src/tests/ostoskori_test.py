f8ef05af82f3d22534d0171d9ac728c4cc366cd6
Pia Manns <pia.manns@gmail.com>
2022-11-26 23:23:22 +0200

    Add test for checking cart product count after adding two different products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,4 +21,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
-  
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        kahvi = Tuote("Kahvi", 5)
+
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kahvi)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
