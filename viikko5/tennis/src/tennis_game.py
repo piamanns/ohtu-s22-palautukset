2bf69a7fb3cbb9df796e46ebbf8b52be4826de98
Pia Manns <pia.manns@gmail.com>
2022-12-03 14:48:52 +0200

    Refactor tennis game scoring count

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,57 +1,50 @@
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.p1_name = player1_name
+        self.p2_name = player2_name
+        self._p1_points = 0
+        self._p2_points = 0
+        self._points_to_win = 4
+        self._diff_to_win = 2
+
+        self._point_descriptions = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty"
+        }
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.p1_name:
+            self._p1_points += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self._p2_points += 1
 
+    def _check_for_win(self, p1_name:str, p1_points:int,
+                       p2_name: str, p2_points:int,
+                       diff_to_win:int):
+
+        diff = abs(p1_points - p2_points)
+        if diff >= diff_to_win:
+            winner = p1_name if p1_points > p2_points else p2_name
+            return f"Win for {winner}"
+        if diff > 0:
+            advantage = p1_name if p1_points > p2_points else p2_name
+            return f"Advantage {advantage}"
+        return "Deuce"
+    
+    def _get_score(self, p1_points, p2_points):
+        if p1_points == p2_points:
+            return f"{self._point_descriptions[p1_points]}-All"
+        return (f"{self._point_descriptions[p1_points]}"
+                f"-{self._point_descriptions[p2_points]}")
+  
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if (self._p1_points >= self._points_to_win
+            or self._p2_points >= self._points_to_win):
+            
+            return self._check_for_win(self.p1_name, self._p1_points,
+                                       self.p2_name, self._p2_points,
+                                       self._diff_to_win)
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+        return self._get_score(self._p1_points, self._p2_points)
