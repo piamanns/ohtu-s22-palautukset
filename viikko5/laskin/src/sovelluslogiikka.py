b20dc2e728d0fdbce73ed6ab371b3494ea6e8a14
Pia Manns <pia.manns@gmail.com>
2022-12-04 22:10:50 +0200

    Implement undo functionality for Laskin

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,6 +1,7 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.edellinen_komento = None
 
     def miinus(self, arvo):
         self.tulos = self.tulos - arvo
@@ -15,32 +16,43 @@ class Sovelluslogiikka:
         self.tulos = arvo
 
 
-class Summa:
-    def __init__(self, sovelluslogiikka, lue_syote):
+class KomentoBase:
+    def __init__(self, sovelluslogiikka, lue_syote=None):
         self._logiikka = sovelluslogiikka
         self._lue_syote = lue_syote
-    
-    def suorita(self):
-        self._logiikka.plus(self._lue_syote())
-
-class Erotus:
-    def __init__(self, sovelluslogiikka, lue_syote):
-        self._logiikka = sovelluslogiikka
-        self._lue_syote = lue_syote
-    
-    def suorita(self):
-        self._logiikka.miinus(self._lue_syote())
-
-class Nollaus:
-    def __init__(self, sovelluslogiikka):
-        self._logiikka = sovelluslogiikka
+        self._edellinen_tulos = 0
 
     def suorita(self):
+        self._edellinen_tulos = self._logiikka.tulos
+        self._logiikka.edellinen_komento = self
+
+    def kumoa(self):
+        self._logiikka.aseta_arvo(self._edellinen_tulos)
+
+
+class Summa(KomentoBase):
+    def suorita(self):
+        syote = self._lue_syote()
+        if syote:
+            super().suorita()
+            self._logiikka.plus(syote)
+
+class Erotus(KomentoBase):    
+    def suorita(self):
+        syote = self._lue_syote()
+        if syote:
+            super().suorita()
+            self._logiikka.miinus(syote)
+
+class Nollaus(KomentoBase):
+    def suorita(self):
+        super().suorita()
         self._logiikka.nollaa()
 
-class Kumoa:
+
+class Kumoa():
     def __init__(self, sovelluslogiikka):
         self._logiikka = sovelluslogiikka
-    
+
     def suorita(self):
-        pass
+        self._logiikka.edellinen_komento.kumoa()
