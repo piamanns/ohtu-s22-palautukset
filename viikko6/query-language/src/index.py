c8fd22b5e1b769caf5d5948596407161da2232b1
Pia Manns <pia.manns@gmail.com>
2022-12-10 15:24:15 +0200

    Add Or matcher

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import All, And, HasAtLeast, HasFewerThan, Not, PlaysIn
+from matchers import All, And, HasAtLeast, HasFewerThan, Or, Not, PlaysIn
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
@@ -37,6 +37,30 @@ def main():
     for player in stats.matches(matcher):
         print(player)
     
+    print("---")
+
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("---")
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
     print("---")
 
     filtered_with_all = stats.matches(All())
