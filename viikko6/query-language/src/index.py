ee4615da758691fc0488e6df8c482a70603534ae
Pia Manns <pia.manns@gmail.com>
2022-12-10 23:00:31 +0200

    Add OneOf-function to QueryBuilder

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -9,14 +9,23 @@ def main():
     stats = Statistics(reader)
 
     query = QueryBuilder()
-    
-    matcher = (
-        query
-        .playsIn("NYR")
-        .hasAtLeast(10, "goals")
-        .hasFewerThan(20, "goals")
+
+    m1 = (
+      query
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
         .build()
     )
+
+    m2 = (
+      query
+        .playsIn("EDM")
+        .hasAtLeast(50, "points")
+        .build()
+    )
+
+    matcher = query.oneOf(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,10 +1,12 @@
-from matchers import All, And, PlaysIn, HasAtLeast, HasFewerThan
+from matchers import All, And, PlaysIn, HasAtLeast, HasFewerThan, Or
 
 
 class QueryBuilder:
-    def __init__(self, matchers=[], new_rule=All()):
-        self._matchers = matchers
-        self._matchers.append(new_rule)
+    def __init__(self, matchers=None, new_rule=None, operator=And):
+        self._matchers = matchers if matchers else []
+        if new_rule:
+            self._matchers.append(new_rule)
+        self._logical_operator = operator
 
     def playsIn(self, team:str):
         return QueryBuilder(self._matchers, PlaysIn(team))
@@ -15,6 +17,11 @@ class QueryBuilder:
     def hasFewerThan(self, value, attr):
         return QueryBuilder(self._matchers, HasFewerThan(value, attr))
 
+    def oneOf(self, m1, m2):
+        return QueryBuilder([m1, m2], None, Or)
+
     def build(self):
-        matcher = And(*self._matchers)
+        if len(self._matchers) == 0:
+            self._matchers.append(All())
+        matcher = self._logical_operator(*self._matchers)
         return matcher
