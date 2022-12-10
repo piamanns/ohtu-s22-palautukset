daa9c84b919ab4e65a7d4bae194dac797b2d3fd5
Pia Manns <pia.manns@gmail.com>
2022-12-10 14:44:26 +0200

    Implement All, Not, HasFewerThan for query language

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,46 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import All, And, HasAtLeast, HasFewerThan, Not, PlaysIn
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+        HasAtLeast(5, "goals"),
+        HasAtLeast(5, "assists"),
+        PlaysIn("PHI")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("---")
+
+    matcher = And(
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
+    )
+    
+    for player in stats.matches(matcher):
+        print(player)
+    
+    print("---")
+
+
+    matcher = And(
+        HasFewerThan(1, "goals"),
+        PlaysIn("NYR")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+    
+    print("---")
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+if __name__ == "__main__":
+    main()
